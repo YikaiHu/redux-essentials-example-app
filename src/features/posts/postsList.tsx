5c0632643e665665fecec29e435aac36f46ec256
Yikai <yikaihu121@outlook.com>
2024-11-09 14:58:53 +0800

    init2

---
+++ src/features/posts/postsList.tsx
@@ -0,0 +1,25 @@
+import { useAppSelector } from '@/app/hooks'
+import { Link } from 'react-router-dom'
+import { selectAllPosts } from './postsSlice'
+import { PostAuthor } from './PostAuthor'
+
+export const PostsList = () => {
+  const posts = useAppSelector(selectAllPosts)
+
+  const renderedPosts = posts.map((post) => (
+    <article className="post-excerpt" key={post.id}>
+      <h3>
+        <Link to={`/posts/${post.id}`}>{post.title}</Link>
+      </h3>
+      <PostAuthor userId={post.user} />
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+    </article>
+  ))
+
+  return (
+    <section className="post-list">
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  )
+}
---
+++ src/features/posts/postsSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice, nanoid, PayloadAction } from '@reduxjs/toolkit'
+import { RootState } from '@/app/store'
+
+export interface Post {
+  id: string
+  title: string
+  content: string
+  user: string
+}
+
+type PostUpdate = Pick<Post, 'id' | 'title' | 'content'>
+
+const initialState: Post[] = [
+  { id: '1', title: 'First Post!', content: 'Hello!', user: '0' },
+  { id: '2', title: 'Second Post', content: 'More text', user: '2' }
+]
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action: PayloadAction<Post>) {
+        state.push(action.payload)
+      },
+      prepare(title: string, content: string, userId: string) {
+        return {
+          payload: { id: nanoid(), title, content, user: userId },
+        }
+      },
+    },
+    postUpdated(state, action: PayloadAction<PostUpdate>) {
+      const { id, title, content } = action.payload
+      const existingPost = state.find((post) => post.id === id)
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
+    },
+  },
+})
+
+export const { postAdded, postUpdated } = postsSlice.actions
+export default postsSlice.reducer
+
+export const selectAllPosts = (state: RootState) => state.posts
+export const selectPostById = (state: RootState, postId: string) => state.posts.find((post) => post.id === postId)
